78f5ade124ae79eb1b5bc46cd6fa95b70e655846
centyh <97asmo@gmail.com>
2021-10-25 14:44:15 +0300

    music volume slider

--- ClickerRyhma3/Assets/Script/Sounds.cs
+++ ClickerRyhma3/Assets/Script/Sounds.cs
@@ -4,9 +4,27 @@ using UnityEngine;
 
 public class Sounds : MonoBehaviour
 {
+
+    private float musicVolume = 1f;
+
     public AudioSource audioSource;
     public AudioClip paperSound;
 
+
+    private void Start()
+    {
+        audioSource.Play();
+    }
+
+    private void Update()
+    {
+        audioSource.volume = musicVolume;
+    }
+
+    public void UpdateVolume(float volume)
+    {
+        musicVolume = volume;
+    }
 
     public void PaperSound()
     {
