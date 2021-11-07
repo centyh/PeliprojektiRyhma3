e74a5350fd83cf83d317369586169471d073e263
centyh <97asmo@gmail.com>
2021-11-07 13:57:06 +0200

    Custom Cursor

--- ClickerRyhma3/Assets/Script/CustomCursor.cs
+++ ClickerRyhma3/Assets/Script/CustomCursor.cs
@@ -5,15 +5,19 @@ using UnityEngine;
 public class CustomCursor : MonoBehaviour
 {
     public Texture2D customCursor;
+
+    private Vector2 cursorHotspot;
     
     void Start()
     {
+        //cursorHotspot = new Vector2(customCursor.width / 2, customCursor.height / 2);
+        //Cursor.SetCursor(customCursor, cursorHotspot, CursorMode.Auto);
         Cursor.SetCursor(customCursor, Vector2.zero, CursorMode.ForceSoftware);
     }
 
     
     void Update()
     {
-        
+
     }
 }
